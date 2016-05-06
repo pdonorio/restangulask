7260ea9c59fd8f6a6021ee61960a89020cd00d30
pdonorio <tempo.pause@gmail.com>
2016-05-06 08:05:53 +0200

    Moving editor for transcriptions inside a card instead of a dialog

--- vanilla/jscode/commons/common.editor.js
+++ vanilla/jscode/commons/common.editor.js
@@ -2,8 +2,8 @@
   'use strict';
 
 angular.module('web')
-    .controller('EditorController', EditorController);
-
+    .controller('EditorController', EditorController)
+    .controller('EditorNonDialogController', EditorNonDialogController);
 
 function EditorController($scope, $sce, $log, $mdDialog, $timeout)
 {
@@ -66,6 +66,61 @@ function EditorController($scope, $sce, $log, $mdDialog, $timeout)
     self.validate = function() {
       // send transcription back as original html
       $mdDialog.hide($sce.getTrustedHtml(self.realHtml));
+    };
+
+};
+
+function EditorNonDialogController(
+    $scope,
+    $sce, $log,
+    //$mdDialog,
+    $timeout)
+{
+    var self = this;
+    $log.debug("TransNonDialog", $scope.currentRecord);
+
+    ////////////////////////////////
+    // CONFIGURE EDITOR WYSIWYG
+    ////////////////////////////////
+
+    self.options = $scope.options;
+    self.options.width = 590;
+    self.options.height = 400;
+
+    // Note: Make sure you using scopes correctly by following this wiki page. If you are having issues with your model not updating, make sure you have a '.' in your model.
+    self.content = null;
+    self.realHtml = null;
+    if ($scope.currentText) {
+        // If i don't use timeout, the content gets wiped
+        $timeout(function () {
+            self.content = angular.copy($scope.currentText); //.getTrustedHtml());
+        }, 800);
+    }
+    //console.log("TEST CONTENT", self.content, $scope);
+
+    $scope.$watch(
+        "edit.content",
+        function handleEditorHtml( newValue, oldValue ) {
+            self.realHtml = $sce.trustAsHtml(oldValue);
+            $log.debug("HTML content watch:",
+                "Old", oldValue, "New", newValue, "real", self.realHtml);
+        }
+    );
+
+    //////////////////////////////
+    // Other functions
+    //////////////////////////////
+
+    self.cancel = function() {
+      //$mdDialog.hide(false);
+      $scope.closeEdit();
+    };
+
+    self.validate = function() {
+      // send transcription back as original html
+      //$mdDialog.hide($sce.getTrustedHtml(self.realHtml));
+      $scope.validateEdit($sce.getTrustedHtml(self.realHtml));
+      $scope.closeEdit();
     };
 
 };
