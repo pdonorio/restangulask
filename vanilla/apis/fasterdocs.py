cf94df73a36c044b6d9cad0dbb51f6db06f74685
pdonorio <tempo.pause@gmail.com>
2016-05-11 08:19:13 +0200

    Adding ui scroll js lib for pagination with elastic search

--- vanilla/apis/fasterdocs.py
+++ vanilla/apis/fasterdocs.py
@@ -7,6 +7,7 @@ Some FAST endpoints implementation
 from __future__ import absolute_import
 from ..base import ExtendedApiResource
 from ..services.elastic import FastSearch
+from .. import decorators as deck
 from ... import get_logger
 
 logger = get_logger(__name__)
@@ -15,16 +16,21 @@ logger = get_logger(__name__)
 class FastDocs(ExtendedApiResource, FastSearch):
     """ A faster search on key values of the database documents """
 
+    @deck.apimethod
     def get(self, searchterms=None):
 
         self.get_instance()
-        data, count = self.fast_get(searchterms)
+        current_element, limit = self.get_paging()
+        # current_page, limit = self.get_paging()
+        # current_element = 1 + (current_page - 1) * limit
+        data, count = self.fast_get(searchterms, current_element, limit)
         return self.response(data, elements=count)
 
 
 class FastSuggestion(ExtendedApiResource, FastSearch):
     """ A faster search on key values of the database documents """
 
+    @deck.apimethod
     def get(self, searchterms=None):
         self.get_instance()
         return self.response(self.fast_suggest(searchterms))
