7975db569e489fbd813338b2b086af8854f300a0
pdonorio <tempo.pause@gmail.com>
2017-01-27 11:18:49 +0100

    Adding back lexique

--- vanilla/jscode/rethink/fast.search.controller.js
+++ vanilla/jscode/rethink/fast.search.controller.js
@@ -36,38 +36,8 @@ function FastSearchController($scope, $log, $stateParams, $timeout,
         }
     });
 
-  self.searchTextChange = function (text) {
-    $log.info('Text changed', text, self.searchText);
-
-    // RELOAD?
-
-    // VERY OLD
-    // $scope.myadapter.reload(0);
-
-    // OLD
-    // $scope.extraits = [];
-    // $scope.myPagingFunction();
-    // $scope.$emit('list:filtered');
-
-    // NEW
-    self.extraits.refresh();
-    // self.loadMore(0);
-  }
-
-  self.clearFilters = function() {
-      self.filters = {};
-      localStorage.removeItem(self.cookieKey);
-      $stateParams.text = '';
-      // self.searchTextChange();
-  }
-
   ///////////////////////////
   // FILTERS
-
-  if ($stateParams.clean) {
-    console.log("Clean parameters");
-    self.clearFilters();
-  }
 
   $scope.advanced = false;
   var filtersKey = [
@@ -133,20 +103,25 @@ function FastSearchController($scope, $log, $stateParams, $timeout,
 
             this.hold = true;
             this.toLoad_ += 5;
-
             var start = this.numLoaded_;
             if (start > 0) start++;
+
+            // console.log("Saving search", self.filters);
+            // Save filters
+            self.filters['searchText'] = self.searchText;
+            localStorage.setItem(self.cookieKey, JSON.stringify(self.filters));
+
+            // Search for lexique
+            if (self.searchText.length > 2) {
+                self.checkLexique();
+            }
 
             SearchService.getDataFast(self.searchText, start, self.filters)
             .then(angular.bind(this, function (obj) {
 
-                // console.log("Saving search", self.filters);
-                // Save filters
-                self.filters['searchText'] = self.searchText;
-                localStorage.setItem(self.cookieKey, JSON.stringify(self.filters));
-
               if (obj && obj.elements > 0) {
                 self.elements = obj.elements;
+
                 // console.log('Data fast:', obj.data, obj.elements);
                 this.items = this.items.concat(obj.data);
 
@@ -164,120 +139,25 @@ function FastSearchController($scope, $log, $stateParams, $timeout,
     }
   }
 
-/*
-  ///////////////////////////
-  self.extraits = []
-  self.extraitsLoop = {
-    numLoaded_: 0,
-    toLoad_: 0,
-    hold_: false,
-    stop_: false,
-
-    getItemAtIndex: function(index) {
-      if (!this.hold_) {
-          // console.log("get item at index", index);
-          if (index > this.numLoaded_) {
-            this.fetchMoreItems_(index, this);
-            return null;
-          }
-      }
-      return index;
-    },
-
-    // For infinite scroll behavior, we always return a slightly higher
-    // number than the previously loaded items.
-    getLength: function() {
-      if (this.stop_ || this.hold_) {
-        return this.numLoaded_;
-      console.log("PAUSE length", this.numLoaded_);
-      } else {
-      // console.log("Get length", this.numLoaded_);
-        return this.numLoaded_ + 5;
-      }
-    },
-
-    refresh : function() {
-        $scope.check = true;
-        self.load = true;
-        this.stop_ = false;
-        self.extraits = [];
-        this.toLoad_ = 0;
-        this.numLoaded_ = 0;
-        // this.items = [];
-    },
-
-    fetchMoreItems_: function(index, reference) {
-
-        reference.hold_ = true;
-        // if (this.toLoad_ < index) {
-          // console.log("MD VR: More", index);
-          this.toLoad_ += 10;
-
-          self.loadMore(reference).then(function(elements) {
-            var loaded = angular.copy(reference.numLoaded_);
-            var toload = angular.copy(reference.toLoad_);
-            console.log("Obtained", elements, loaded, toload);
-
-            if (elements < reference.toLoad_) {
-                reference.numLoaded_ = elements;
-                console.log("Should stop");
-                reference.stop_ = true;
-            } else {
-                reference.numLoaded_ = reference.toLoad_;
-            }
-            self.load = false;
-            reference.hold_ = false;
-            // console.log("LOADED", reference.numLoaded_, reference.toLoad_);
-          });
-
-        // }
-
-    }
-  };
-
-  $scope.check = false;
-  self.loadMore = function(reference) {
-
-    reference.hold_ = true;
-    // Note: loading 10 at the time
-
-    self.filters['searchText'] = self.searchText;
-    localStorage.setItem(self.cookieKey, JSON.stringify(self.filters));
-    var start = reference.numLoaded_;
-    if (start > 0)
-        start++;
-
-    console.log("INDEX", start);
-    return SearchService.getDataFast(
-      self.searchText, start, self.filters).then(
-        function (out) {
-          // console.log('Data fast:', out.data, out.elements);
-          if (out && out.elements) {
-              self.elements = null;
-              $scope.check = false;
-
-              // // Search for lexique
-              // if (self.searchText.length > 2) {
-              //     self.checkLexique();
-              // }
-
-              if (out.elements) {
-                self.elements = out.elements;
-                forEach(out.data, function(element, index) {
-                    console.log("push", element._source.extrait)
-                    self.extraits.push(element);
-                });
-                console.log("Received", out.data);
-              }
-          } else {
-              $scope.check = true;
-              self.elements = self.extraits.length;
-          }
-          console.log("Total", self.extraits);
-          return self.elements;
-        });
+  self.searchTextChange = function (text) {
+    $log.info('Text changed', text, self.searchText);
+    // RELOAD
+    self.extraits.refresh();
   }
-*/
+
+  self.clearFilters = function() {
+      self.filters = {};
+      localStorage.removeItem(self.cookieKey);
+      $stateParams.text = '';
+      // self.searchTextChange();
+      // RELOAD
+      self.extraits.refresh();
+  }
+
+  if ($stateParams.clean) {
+    console.log("Clean parameters");
+    self.clearFilters();
+  }
 
   self.querySearch = function (text) {
 
@@ -310,14 +190,13 @@ function FastSearchController($scope, $log, $stateParams, $timeout,
   }
 
   self.checkLexique = function () {
-    // console.log("CHECK LEX");
     $scope.rows = null;
     $scope.selected = self.searchText;
     $scope.fields = [
         'sheet', 'macro', 'micro',
         'titre', 'latin', 'français', 'italiano'
     ];
-    // console.log("TEST", selection);
+    console.log("CHECK LEX", $scope.selected);
 
     SearchService.getFastLex(self.searchText)
       .then(function(out){
