639c91baaad90216f5d28850ec17ddbf3e1c86e8
pdonorio <tempo.pause@gmail.com>
2017-01-27 11:19:01 +0100

    Remove useless import of flask table

--- frontend/felask/basemodel.py
+++ frontend/felask/basemodel.py
@@ -6,10 +6,10 @@ import importlib
 from datetime import datetime
 from config import user_config
 from collections import OrderedDict
-from flask import url_for, request
-from flask_table import Table, Col, create_table
+# from flask import url_for, request
+# from flask_table import Table, Col, create_table
 from flask_login import LoginManager
-from sqlalchemy import inspect
+# from sqlalchemy import inspect
 from flask_sqlalchemy import SQLAlchemy
 
 
@@ -69,52 +69,52 @@ class User(db.Model):
 
 #############################################
 # Convert an SQLALCHEMY model into a Flask table
-class AnchorCol(Col):
-    def td_format(self, content):
-        return '<a href="/view/' + content + '">' + content + '</a>'
+# class AnchorCol(Col):
+#     def td_format(self, content):
+#         return '<a href="/view/' + content + '">' + content + '</a>'
 
 
-class ItemTable(Table):
+# class ItemTable(Table):
 
-    def sort_url(self, col_key, reverse=False):
-        if reverse:
-            direction = 'desc'
-        else:
-            direction = 'asc'
-        return url_for(request.endpoint, sort=col_key, direction=direction)
-
-
-def model2list(obj):
-    mylist = []
-    for column in inspect(obj).attrs:
-        mylist.append(column.key)
-    return mylist
+#     def sort_url(self, col_key, reverse=False):
+#         if reverse:
+#             direction = 'desc'
+#         else:
+#             direction = 'asc'
+#         return url_for(request.endpoint, sort=col_key, direction=direction)
 
 
-def model2table(obj, selected):
-    """ Give me an SQLALCHEMY obj to get an HTML table """
+# def model2list(obj):
+#     mylist = []
+#     for column in inspect(obj).attrs:
+#         mylist.append(column.key)
+#     return mylist
 
-    table_name = 'Table' + obj.__name__
-    TableCls = create_table(table_name, base=ItemTable)
-    mapper = inspect(obj)
 
-    for column in mapper.attrs:
+# def model2table(obj, selected):
+#     """ Give me an SQLALCHEMY obj to get an HTML table """
 
-        # What to skip
-        if selected and column.key not in selected:
-            continue
+#     table_name = 'Table' + obj.__name__
+#     TableCls = create_table(table_name, base=ItemTable)
+#     mapper = inspect(obj)
 
-        colname = column.key.replace('_', ' ').capitalize()
-        if column.key == 'id':  # 'patient_id':
-            TableCls.add_column(column.key, AnchorCol(colname))
-        else:
-            TableCls.add_column(column.key, Col(colname))
+#     for column in mapper.attrs:
 
-    TableCls.classes = ['table', 'table-hover']
-    TableCls.thead_classes = ["thead-default"]
-    TableCls.allow_sort = True
+#         # What to skip
+#         if selected and column.key not in selected:
+#             continue
 
-    return TableCls
+#         colname = column.key.replace('_', ' ').capitalize()
+#         if column.key == 'id':  # 'patient_id':
+#             TableCls.add_column(column.key, AnchorCol(colname))
+#         else:
+#             TableCls.add_column(column.key, Col(colname))
+
+#     TableCls.classes = ['table', 'table-hover']
+#     TableCls.thead_classes = ["thead-default"]
+#     TableCls.allow_sort = True
+
+#     return TableCls
 
 ##################################################
 # WHERE THE MAGIC HAPPENS: dynamic models, and tables
@@ -131,10 +131,10 @@ insertable = user_config['models'].get('insert_fields')
 extra_selected = user_config['models'].get('extra_fields_to_show', [])
 if insertable:
     selected = extra_selected + insertable
-# If configuration has no options
-else:
-    selected = model2list(MyModel)
-    insertable = selected
+# # If configuration has no options
+# else:
+#     selected = model2list(MyModel)
+#     insertable = selected
 
-# Build the main table for the view
-MyTable = model2table(MyModel, selected)
+# # Build the main table for the view
+# MyTable = model2table(MyModel, selected)
