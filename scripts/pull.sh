8d32b3be1004d3e0447c8d041a4d2c9caf97e9db
pdonorio <tempo.pause@gmail.com>
2017-02-15 10:43:27 +0100

    Bug fix

--- scripts/pull.sh
+++ scripts/pull.sh
@@ -1,45 +1,45 @@
 #!/bin/bash
 
-# ################################
-# services="backend"
-# #services="angulask rest-mock"
-# com="git"
-# pull="$com pull"
-# current_branch="rethink"
+################################
+services="backend"
+#services="angulask rest-mock"
+com="git"
+pull="$com pull"
+current_branch="rethink"
 
-# $pull origin $current_branch
-# if [ "$?" != "0" ]; then exit 1; fi
+$pull origin $current_branch
+if [ "$?" != "0" ]; then exit 1; fi
 
-# for service in $services;
-# do
-#     echo "Repo '$service'"
-#     cd $service
-#     $pull
-#     if [ "$?" != "0" ]; then exit 1; fi
-#     cd ..
-# done
+for service in $services;
+do
+    echo "Repo '$service'"
+    cd $service
+    $pull
+    if [ "$?" != "0" ]; then exit 1; fi
+    cd ..
+done
 
-# ################################
-# # echo "Submodule sync"
-# # git submodule sync
-# # git submodule update
+################################
+# echo "Submodule sync"
+# git submodule sync
+# git submodule update
 
-# ################################
-# cd containers
-# docker-compose \
-#     -f docker-compose.yml \
-#     -f custom/rethink.yml \
-#     -f development.yml \
-#     -f production.yml \
-#     pull
+################################
+cd containers
+docker-compose \
+    -f docker-compose.yml \
+    -f custom/rethink.yml \
+    -f development.yml \
+    -f production.yml \
+    pull
 
-# if [ "$?" != "0" ]; then exit 1; fi
+if [ "$?" != "0" ]; then exit 1; fi
 
-# ################################
-# docker-compose run bower /bin/sh -c "bower update"
+################################
+docker-compose run bower /bin/sh -c "bower update"
 
-# if [ "$?" != "0" ]; then exit 1; fi
-# cd ..
+if [ "$?" != "0" ]; then exit 1; fi
+cd ..
 
 ################################
 pwd
