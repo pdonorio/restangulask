7dbe005dd52efaf92d330373426ba65eb4bd73c7
pdonorio <tempo.pause@gmail.com>
2017-05-23 17:13:02 +0200

    fixing build letsencrypt. to be tested...

--- builds/sslproxy/letsencrypt.sh
+++ builds/sslproxy/letsencrypt.sh
@@ -1,17 +1,37 @@
-# !/bin/sh
+# !/bin/bash
+set -e
 
 # DOMAIN="b2stage.cineca.it"
 # MODE="--staging"
 # MODE=""
 
 # Renewall script
-echo "Mode *$MODE* and DOMAIN $DOMAIN"
-./acme.sh --issue --debug -d $DOMAIN -w $WWWDIR $MODE
-echo "Completed. Check:"
-./acme.sh --list
+echo "Mode: *$MODE*"
+echo "Domain: $DOMAIN"
 
-echo "Copy files"
-cp $DOMAIN/$DOMAIN.key $CERTDIR/privkey1.pem
-cp $DOMAIN/fullchain.cer $CERTDIR/fullchain1.pem
+./acme.sh --issue --debug \
+    --fullchain-file $CERTDIR/fullchain1.pem \
+    --key-file $CERTDIR/privkey1.pem \
+    -d $DOMAIN -w $WWWDIR $MODE
 
-nginx -s reload
+if [ "$?" == "0" ]; then
+    # List what we have
+    echo "Completed. Check:"
+    ./acme.sh --list
+
+    nginx -s reload
+else
+    echo "ACME FAILED!"
+fi
+
+# # Renewall script
+# echo "Mode *$MODE* and DOMAIN $DOMAIN"
+# ./acme.sh --issue --debug -d $DOMAIN -w $WWWDIR $MODE
+# echo "Completed. Check:"
+# ./acme.sh --list
+
+# echo "Copy files"
+# cp $DOMAIN/$DOMAIN.key
+# cp $DOMAIN/fullchain.cer
+
+# nginx -s reload
